69416e2c44e561fdf332ab443bb046faccfb83c8
SloppyJoeJonsen <ati_64@hotmail.de>
2025-10-30 01:52:47 +0100

    Soundblaster headphone fix

--- hosts/sloppy/configuration.nix
+++ hosts/sloppy/configuration.nix
@@ -1,21 +1,56 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   imports =
     [ ./variables.nix ../../nixos/shared.nix ./hardware-configuration.nix ];
+home-manager.users."${config.var.username}" = import ./home.nix;
 
-  home-manager.users."${config.var.username}" = import ./home.nix;
+# Enable ALSA state restoration
+hardware.alsa.enablePersistence = true;
 
-  services.udev.extraRules = ''
+# ALSA restore that waits for PipeWire to be ready
+systemd.services.alsa-restore-after-pipewire = {
+  description = "Restore Sound Card State After PipeWire";
+  wantedBy = [ "multi-user.target" ];
+  after = [ "sound.target" "alsa-store.service" ];
+  serviceConfig = {
+    Type = "oneshot";
+    ExecStart = pkgs.writeShellScript "restore-alsa-smart" ''
+      # Wait for PipeWire user service to be active (max 60 seconds)
+      for i in {1..60}; do
+        if systemctl --user -M ${config.var.username}@ is-active pipewire.service >/dev/null 2>&1; then
+          echo "PipeWire is active, waiting 3 more seconds for full initialization..."
+          sleep 3
+          ${pkgs.alsa-utils}/bin/alsactl restore
+          echo "ALSA state restored!"
+          exit 0
+        fi
+        echo "Waiting for PipeWire... ($i/60)"
+        sleep 1
+      done
+      echo "Timeout waiting for PipeWire, restoring anyway..."
+      ${pkgs.alsa-utils}/bin/alsactl restore
+    '';
+    RemainAfterExit = true;
+  };
+};
+
+services.udev.extraRules = ''
     # OBS virtual camera
     KERNEL=="video[0-9]*", GROUP="video", MODE="0666"
-
     # Logitech devices for Solaar
     SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", MODE="0666"
     SUBSYSTEM=="hidraw", ATTRS{idVendor}=="046d", MODE="0666"
-      # Aquacomputer devices
+    # Aquacomputer devices
     SUBSYSTEM=="usb", ATTRS{idVendor}=="0c70", MODE="0666"
     KERNEL=="hidraw*", ATTRS{idVendor}=="0c70", MODE="0666"
-  '';
-
-  # Don't touch this
-  system.stateVersion = "24.05";
+    # Sound Blaster Creative always as card 0
+    SUBSYSTEM=="sound", KERNEL=="card*", ATTRS{vendor}=="0x1102", ATTR{number}="0"
+    # AMD Navi HDMI as card 1
+    SUBSYSTEM=="sound", KERNEL=="card*", DEVPATH=="*/0000:03:00.1/*", ATTR{number}="1"
+    # AMD Rembrandt HDMI as card 2
+    SUBSYSTEM=="sound", KERNEL=="card*", DEVPATH=="*/0000:13:00.1/*", ATTR{number}="2"
+    # AMD HD Audio as card 3
+    SUBSYSTEM=="sound", KERNEL=="card*", DEVPATH=="*/0000:13:00.6/*", ATTR{number}="3"
+'';
+# Don't touch this
+system.stateVersion = "24.05";
 }
--- hosts/sloppy/home.nix
+++ hosts/sloppy/home.nix
@@ -1,23 +1,36 @@
 { pkgs, config, ... }: {
-
   imports = [
-    ./variables.nix
-    ../../home/shared.nix
-    ../../home/essentials.nix
-    # ./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
+./variables.nix
+../../home/shared.nix
+../../home/essentials.nix
+# ./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
   ];
-
-  home = {
-    inherit (config.var) username;
-    homeDirectory = "/home/" + config.var.username;
-
-    packages = with pkgs; [ ];
-
-    file.".face.icon" = { source = ../profile_picture.png; };
-
-    # Don't touch this
-    stateVersion = "24.05";
+home = {
+inherit (config.var) username;
+homeDirectory = "/home/" + config.var.username;
+packages = with pkgs; [ ];
+file.".face.icon" = { source = ../profile_picture.png; };
+# Don't touch this
+stateVersion = "24.05";
   };
+programs.home-manager.enable = true;
 
-  programs.home-manager.enable = true;
+# Autostart script to restore ALSA after login
+systemd.user.services.alsa-restore-user = {
+  Unit = {
+    Description = "Restore ALSA State After Login";
+    After = [ "default.target" ];
+  };
+  Service = {
+    Type = "oneshot";
+    ExecStart = "${pkgs.writeShellScript "restore-alsa" ''
+      sleep 5
+      ${pkgs.alsa-utils}/bin/alsactl restore
+    ''}";
+    RemainAfterExit = true;
+  };
+  Install = {
+    WantedBy = [ "default.target" ];
+  };
+};
 }
