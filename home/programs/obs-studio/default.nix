a51e9f38f6d3dea49a837eb68514033d8c297c8b
SloppyJoeJonsen <ati_64@hotmail.de>
2025-11-01 18:22:46 +0100

    pipewire/obs fix

--- home/programs/obs-studio/default.nix
+++ home/programs/obs-studio/default.nix
@@ -8,10 +8,16 @@
       obs-vkcapture
     ];
   };
-
+  
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "sleep 5 && rm -rf ~/.config/obs-studio/.sentinel && env QT_QPA_PLATFORM=xcb obs --startreplaybuffer --minimize-to-tray"
     ];
+    
+    bind = [
+      "ALT, F10, exec, ${pkgs.obs-cmd}/bin/obs-cmd -w obsws://localhost:4455 replay save"
+    ];
   };
+  
+  home.packages = [ pkgs.obs-cmd ];
 }
