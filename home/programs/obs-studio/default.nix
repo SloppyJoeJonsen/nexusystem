078c89db1b26f64b9da03de47357e7c0854d0ad6
SloppyJoeJonsen <ati_64@hotmail.de>
2025-11-14 23:57:50 +0100

    bissl. sauber gemacht und Obs fix

--- home/programs/obs-studio/default.nix
+++ home/programs/obs-studio/default.nix
@@ -9,10 +9,10 @@
     ];
   };
   
-  wayland.windowManager.hyprland.settings = {
-    exec-once = [
-      "sleep 5 && rm -rf ~/.config/obs-studio/.sentinel && env QT_QPA_PLATFORM=xcb obs --startreplaybuffer --minimize-to-tray"
-    ];
+        wayland.windowManager.hyprland.settings = {
+          exec-once = [
+            "sleep 5 && rm -rf ~/.config/obs-studio/.sentinel && obs --startreplaybuffer --minimize-to-tray"
+          ];
     
     bind = [
       "ALT, F10, exec, ${pkgs.obs-cmd}/bin/obs-cmd -w obsws://localhost:4455 replay save"
